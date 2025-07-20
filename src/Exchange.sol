f729aa4938348f5b4366ec9b2b15c5051c196088
mandip-kamaliya <mandipkamaliya7@gmail.com>
2025-07-20 03:26:08 +0000

    exchange contract added

---
+++ src/Exchange.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "./ERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract Exchange is ERC20,ReentrancyGuard {
+    
+
+    
+}
