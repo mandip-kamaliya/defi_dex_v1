e29877ac7db145dfb315cabe4dd19ce1fe3adc80
mandip-kamaliya <mandipkamaliya7@gmail.com>
2025-07-21 03:27:42 +0000

    Fix:critical math error in exchange contract

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 
 import "./ERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Exchange is ERC20, ReentrancyGuard {
     //state variable
@@ -35,7 +36,7 @@ contract Exchange is ERC20, ReentrancyGuard {
         returns (uint256)
     {
         require(inputreserve > 0 && inputamount > 0, "invalid value provided!!");
-        uint256 inputwithfee = inputreserve * 997;
+        uint256 inputwithfee = inputamount * 997;
         uint256 numerator = (inputwithfee * outputreserve);
         uint256 denominator = ((inputreserve * 1000) + inputwithfee);
         return numerator / denominator;
@@ -56,18 +57,23 @@ contract Exchange is ERC20, ReentrancyGuard {
     //liquidity functions
 
     function addliquidity(uint256 tokenadded) external payable nonReentrant returns (uint256) {
+        require(msg.value > 0 && tokenadded > 0, "Invalid values provided");
         uint256 ethBalance = address(this).balance;
         uint256 tokenreserve = gettokenreserve();
 
         if (tokenreserve == 0) {
             require(IERC20(tokenaddress).balanceOf(msg.sender) >= tokenadded, "insufficient balance");
             IERC20(tokenaddress).transferFrom(msg.sender, address(this), tokenadded);
-            uint256 liquidity = ethBalance;
+            uint256 liquidity = msg.value;
             _mint(msg.sender, liquidity);
             emit AddedLiquidity(msg.sender, msg.value, tokenadded);
             return liquidity;
         } else {
-            uint256 liquidity = (msg.value * totalSupply()) / ethBalance;
+            uint256 liquidity = (msg.value * totalSupply()) / (ethBalance - msg.value);
+            uint256 currentEthReserve = ethBalance - msg.value;
+            uint256 expectedTokensRequired = (msg.value * tokenreserve) / currentEthReserve;
+            require(tokenadded == expectedTokensRequired, "Disproportionate token amount for liquidity");
+
             require(IERC20(tokenaddress).balanceOf(msg.sender) >= tokenadded, "insufficient balance");
             IERC20(tokenaddress).transferFrom(msg.sender, address(this), tokenadded);
             _mint(msg.sender, liquidity);
@@ -78,12 +84,12 @@ contract Exchange is ERC20, ReentrancyGuard {
 
     function removeliquidity(uint256 tokenamount) external nonReentrant returns (uint256, uint256) {
         require(tokenamount > 0, "token amount should be more than zero");
-
+        require(totalSupply() > 0, "No liquidity in pool to remove");
         uint256 ethamount = (tokenamount * address(this).balance) / totalSupply();
-        uint256 tokenvalue = (tokenamount * IERC20(tokenaddress).balanceOf(address(this)));
+        uint256 tokenvalue = (tokenamount * IERC20(tokenaddress).balanceOf(address(this))) / totalSupply();
         _burn(msg.sender, tokenamount);
         payable(msg.sender).transfer(ethamount);
-        IERC20(tokenaddress).transferFrom(address(this), msg.sender, tokenvalue);
+        IERC20(tokenaddress).transfer(msg.sender, tokenvalue);
         emit RemovedLiquidity(msg.sender, ethamount, tokenvalue);
         return (ethamount, tokenvalue);
     }
