66b4e363c39dda2da629d48fc0bfd336abc38818
mandip-kamaliya <mandipkamaliya7@gmail.com>
2025-07-22 14:08:09 +0000

    update ERC20.sol file

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
     constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+        require(initialOwner != address(0), "MyToken: zero address");
+        _mint(initialOwner, 10 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
