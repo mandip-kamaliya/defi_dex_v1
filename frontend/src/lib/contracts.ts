cbb5a206611457ba7036ba7c1bc9454045d15eef
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2025-12-14 16:00:37 +0530

    added src/lib/contracts.ts file

--- frontend/src/lib/contracts.ts
+++ frontend/src/lib/contracts.ts
@@ -1,70 +1,103 @@
 export const EXCHANGE_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3' // Replace with your deployed contract address
+export const FACTORY_ADDRESS = '0xE2fa192e962C41B2895F4F3c8865bF045dC07A57' // Deployed Factory address on Sepolia
 
 export const EXCHANGE_ABI = [
   {
     "inputs": [
-      {"internalType": "address", "name": "_tokenA", "type": "address"},
-      {"internalType": "address", "name": "_tokenB", "type": "address"}
+      {"internalType": "uint256", "name": "minTokens", "type": "uint256"},
+      {"internalType": "address", "name": "recipient", "type": "address"}
     ],
-    "name": "createPool",
-    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "name": "swapEthForTokens",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "tokensSold", "type": "uint256"},
+      {"internalType": "uint256", "name": "minEth", "type": "uint256"}
+    ],
+    "name": "tokenForEthSwap",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
     "stateMutability": "nonpayable",
     "type": "function"
   },
   {
     "inputs": [
-      {"internalType": "address", "name": "_tokenA", "type": "address"},
-      {"internalType": "address", "name": "_tokenB", "type": "address"},
-      {"internalType": "uint256", "name": "_amountADesired", "type": "uint256"},
-      {"internalType": "uint256", "name": "_amountBDesired", "type": "uint256"},
-      {"internalType": "uint256", "name": "_amountAMin", "type": "uint256"},
-      {"internalType": "uint256", "name": "_amountBMin", "type": "uint256"},
-      {"internalType": "address", "name": "_to", "type": "address"},
-      {"internalType": "uint256", "name": "_deadline", "type": "uint256"}
+      {"internalType": "uint256", "name": "tokenadded", "type": "uint256"}
     ],
-    "name": "addLiquidity",
+    "name": "addliquidity",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "tokenamount", "type": "uint256"}
+    ],
+    "name": "removeliquidity",
     "outputs": [
-      {"internalType": "uint256", "name": "amountA", "type": "uint256"},
-      {"internalType": "uint256", "name": "amountB", "type": "uint256"},
-      {"internalType": "uint256", "name": "liquidity", "type": "uint256"}
+      {"internalType": "uint256", "name": "", "type": "uint256"},
+      {"internalType": "uint256", "name": "", "type": "uint256"}
     ],
     "stateMutability": "nonpayable",
     "type": "function"
   },
   {
-    "inputs": [
-      {"internalType": "address", "name": "_tokenA", "type": "address"},
-      {"internalType": "address", "name": "_tokenB", "type": "address"},
-      {"internalType": "uint256", "name": "_amountIn", "type": "uint256"},
-      {"internalType": "uint256", "name": "_amountOutMin", "type": "uint256"},
-      {"internalType": "address[]", "name": "_path", "type": "address[]"},
-      {"internalType": "address", "name": "_to", "type": "address"},
-      {"internalType": "uint256", "name": "_deadline", "type": "uint256"}
-    ],
-    "name": "swapExactTokensForTokens",
-    "outputs": [{"internalType": "uint256[]", "name": "amounts", "type": "uint256[]"}],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {"internalType": "address", "name": "_tokenA", "type": "address"},
-      {"internalType": "address", "name": "_tokenB", "type": "address"}
-    ],
-    "name": "getPool",
-    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "inputs": [],
+    "name": "gettokenreserve",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
     "stateMutability": "view",
     "type": "function"
   },
   {
     "inputs": [
-      {"internalType": "uint256", "name": "_amountIn", "type": "uint256"},
-      {"internalType": "uint256", "name": "_reserveIn", "type": "uint256"},
-      {"internalType": "uint256", "name": "_reserveOut", "type": "uint256"}
+      {"internalType": "uint256", "name": "inputamount", "type": "uint256"},
+      {"internalType": "uint256", "name": "inputreserve", "type": "uint256"},
+      {"internalType": "uint256", "name": "outputreserve", "type": "uint256"}
     ],
-    "name": "getAmountOut",
-    "outputs": [{"internalType": "uint256", "name": "amountOut", "type": "uint256"}],
+    "name": "getamount",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
     "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "tokensSold", "type": "uint256"}
+    ],
+    "name": "getEthfortokens",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "uint256", "name": "Ethsold", "type": "uint256"}
+    ],
+    "name": "gettokenforEth",
+    "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const
+
+export const FACTORY_ABI = [
+  {
+    "inputs": [
+      {"internalType": "address", "name": "_tokenAddress", "type": "address"}
+    ],
+    "name": "createNewExchange",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {"internalType": "address", "name": "_tokenAddress", "type": "address"}
+    ],
+    "name": "getExchange",
+    "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+    "stateMutability": "view",
     "type": "function"
   }
 ] as const
@@ -130,9 +163,9 @@ export const ERC20_ABI = [
   }
 ] as const
 
-// Sample token addresses (replace with your actual token addresses)
+// Token addresses for your DEX
 export const TOKENS = {
-  WETH: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', // Hardhat default account
-  USDC: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', // Hardhat account 2
-  DAI: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', // Hardhat account 3
+  ETH: '0x0000000000000000000000000000000000000000', // Native ETH
+  USDC: '0xbE47c51b571685d6B85dd6AaEb23E89403297a92', // Deployed MyToken on Sepolia
+  DAI: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', // Replace with another deployed ERC20 token
 } as const
